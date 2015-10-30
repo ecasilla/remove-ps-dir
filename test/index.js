4f5a17fe24f3c90ed31928d59ace1cf7c7358853
Ernie Casilla <ecasilla@icloud.com>
2015-10-29 22:00:56 -0300

    fixed all eslint issues

--- test/index.js
+++ test/index.js
@@ -3,50 +3,46 @@
 var expect = require('chai').expect;
 var sinon = require('sinon');
 var RemovePsDir = require('../lib');
-var removePsDir = RemovePsDir();
 
 describe('remove-ps-dir', function () {
   context('Interface', function () {
     it('RemovePsDir should be a object', function (){
-      expect(RemovePsDir()).to.be.an('object');
+      expect(RemovePsDir).to.be.an('object');
     });
-    it('should be an instance of a RemovePsDir object', function (){
-      expect(removePsDir).to.be.instanceof(RemovePsDir);
+    it('should have a private remove fn', function (){
+      expect(RemovePsDir.remove).to.be.an('function');
     });
-    it('should have a private remove fn', function(){
-      expect(removePsDir.remove).to.be.an('function');
+    it('should have a private gatekeeper fn', function (){
+      expect(RemovePsDir.__gateKeeper).to.be.an('function');
     });
-    it('should have a private gatekeeper fn', function(){
-      expect(removePsDir.__gateKeeper).to.be.an('function');
+    it('should have a private getPsDir fn', function (){
+      expect(RemovePsDir.__getPsDir).to.be.an('function');
     });
-    it('should have a private getPsDir fn', function(){
-      expect(removePsDir.__getPsDir).to.be.an('function');
+    it('should have a private Filter Paths fn', function (){
+      expect(RemovePsDir.__filterPaths).to.be.an('function');
     });
-    it('should have a private Filter Paths fn', function(){
-      expect(removePsDir.__filterPaths).to.be.an('function');
+    it('should have a private Map fn', function (){
+      expect(RemovePsDir.__map).to.be.an('function');
     });
-    it('should have a private Map fn', function(){
-      expect(removePsDir.__map).to.be.an('function');
-    });
-    it('should have a private Running Processes fn', function(){
-      expect(removePsDir.__runningProcess).to.be.an('function');
+    it('should have a private Running Processes fn', function (){
+      expect(RemovePsDir.__runningProcess).to.be.an('function');
     });
   });
-  context('Implementation',function() {
-    it('should guard against bad args being passed by calling the gatekeeper', function(){
-      var spy = sinon.spy(removePsDir,'__gateKeeper');
-      removePsDir.remove()
+  context('Implementation', function () {
+    it('should guard against bad args being passed by calling the gatekeeper', function (){
+      var spy = sinon.spy(RemovePsDir, '__gateKeeper');
+      RemovePsDir.remove();
       expect(spy.called).to.eql(true);
     });
     it('should return an Error if path is not defined', function (){
-      expect(removePsDir.__gateKeeper()).to.be.an.instanceof(Error);
+      expect(RemovePsDir.__gateKeeper()).to.be.an.instanceof(Error);
     });
     it('should return an Error if a callback is not passed', function (){
-      expect(removePsDir.__gateKeeper('',undefined)).to.be.an.instanceof(Error);
+      expect(RemovePsDir.__gateKeeper('', undefined)).to.be.an.instanceof(Error);
     });
-    it('should return an a array of number only dirs for a given file path', function(){
-       var root = process.cwd();
-       //expect(removePsDir.__getPsDir(root + '/test/fixtures/tmp')).to.eql(['12345','66609']);
+    it('should return an a array of number only dirs for a given file path', function (){
+       //var root = process.cwd();
+       //expect(RemovePsDir.__getPsDir(root + '/test/fixtures/tmp')).to.eql(['12345','66609']);
     });
   });
 });
